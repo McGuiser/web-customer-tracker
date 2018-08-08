2cdb84541b53d67806d19aece399635923d527d5
Corey Castillo <mcguiser@users.noreply.github.com>
2018-08-08 16:28:32 -0500

    Added Before advice and fixed pointcut

--- src/com/corey/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/corey/springdemo/aspect/CRMLoggingAspect.java
@@ -2,7 +2,9 @@ package com.corey.springdemo.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -14,19 +16,29 @@ public class CRMLoggingAspect {
 	private Logger myLogger = Logger.getLogger(getClass().getName());
 	
 	// Setup pointcut declarations
-	@Pointcut("execution(* com.corey.springdemo.controller.*.*.(..))")
+	@Pointcut("execution(* com.corey.springdemo.controller.*.*(..))")
 	private void forControllerPackage() {}
 	
-	@Pointcut("execution(* com.corey.springdemo.service.*.*.(..))")
+	@Pointcut("execution(* com.corey.springdemo.service.*.*(..))")
 	private void forServicePackage() {}
 	
-	@Pointcut("execution(* com.corey.springdemo.dao.*.*.(..))")
+	@Pointcut("execution(* com.corey.springdemo.dao.*.*(..))")
 	private void forDaoPackage() {}
 	
-	@Pointcut("forControllerPackage || forServicePackage || forDaoPackage")
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
 	private void forAppFlow() {}
 	
 	// Add @Before advice
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		
+		// Display method we are calling
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info(theMethod);
+		
+		// Display the arguments to the method
+		
+	}
 	
 	// Add @AfterReturning advice
 
